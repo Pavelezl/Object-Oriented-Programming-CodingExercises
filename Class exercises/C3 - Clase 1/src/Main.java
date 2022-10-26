4af64b6f625759fe80ccc70f69a1087727d33d6d
Pablo Velez <pablovelezlopera3@gmail.com>
2022-10-25 19:49:44 -0500

    Class1

---
+++ Class exercises/C3 - Clase 1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Console.log
+        System.out.println("Hello world!");
+
+        //Variables
+
+        String nombre = "Pablo";
+        Integer edad = 19;
+        Double altura = 1.75;
+
+        //Operaciones
+
+        System.out.println("mi nombre es " + nombre + " y mi altura es de " + altura + " metros");
+
+        //Condicionales
+
+        if(altura > 1.40){
+            System.out.println("Puede subir");
+        }else{
+            System.out.println("no puede subir");
+        }
+
+    }
+}
---
+++ Classwork/C3 - Mesa 1/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        String queEs1 = "perro";
+        String nombre1 = "Manchitas";
+        Integer edad1 = 2;
+        Double cantComida1 = 1.5;
+        String preguntas1 = "woof";
+
+        String queEs2 = "pez";
+        String nombre2 = "Nemo";
+        Integer edad2 = 1;
+        Double cantComida2 = 0.14;
+        String preguntas2 = "glugh";
+
+        String queEs3 = "catto";
+        String nombre3 = "Silvestre";
+        Integer edad3 = 3;
+        Double cantComida3 = 0.5;
+        String preguntas3 = "mew";
+
+        String queEs4 = "tortuga";
+        String nombre4 = "manuelita";
+        Integer edad4 = 12;
+        Double cantComida4 = 0.3;
+        String preguntas4 = "pehuajo";
+
+        String queEs5 = "pajaro";
+        String nombre5 = "gardel";
+        Integer edad5 = 1;
+        Double cantComida5 = 0.35;
+        String preguntas5 = "piopio";
+
+        System.out.println(nombre1 + " tiene " + edad1 + " años");
+        System.out.println(nombre1 + " come " + cantComida1 + " kilos de comida y hace "+preguntas1);
+
+        System.out.println(nombre2 + " tiene " + edad2 + " años");
+        System.out.println(nombre2 + " come " + cantComida2 + " kilos de comida y hace "+preguntas2);
+
+        System.out.println(nombre3 + " tiene " + edad3 + " años");
+        System.out.println(nombre3 + " come " + cantComida3 + " kilos de comida y hace "+preguntas3);
+
+        System.out.println(nombre4 + " tiene " + edad4 + " años");
+        System.out.println(nombre4 + " come " + cantComida4 + " kilos de comida y hace "+preguntas4);
+
+        System.out.println(nombre5 + " tiene " + edad5 + " años");
+        System.out.println(nombre5 + " come " + cantComida5 + " kilos de comida y hace "+preguntas5);
+
+    }
+}
