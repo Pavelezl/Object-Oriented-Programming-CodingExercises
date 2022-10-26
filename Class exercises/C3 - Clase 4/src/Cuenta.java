9c2991694be013044a3f01009746e0d414ab4b89
Pablo Velez <pablovelezlopera3@gmail.com>
2022-10-26 16:13:41 -0500

    Class 4: Objects and UML

---
+++ Class exercises/C3 - Clase 4/src/Cuenta.java
@@ -0,0 +1,9 @@
+public class Cuenta {
+
+    private String numeroDeCuenta;
+    private Double saldo;
+
+    public Cuenta(Double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ Class exercises/C3 - Clase 4/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cuenta cuenta = new Cuenta(10000.0);
+    }
+
+
+}
